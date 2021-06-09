e4a3803b527c3e1f1d9dba19466d7d0679d2e72d
Rahul Aedula <rahulaedula95@gmail.com>
2021-06-09 11:44:10 -0600

    Week 2

---
+++ Week-2/example2.cpp
@@ -0,0 +1,31 @@
+// This is a comment. 
+// Anything written in the same line after two back slashes will not be read as code to the compiler.
+// Comments are primarily used as ways to write short descriptions to help developers.
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    // Let us create some variables.
+
+    int a = 10;
+
+    // if you want to change the value of a
+    
+    a = 20;
+
+
+    cout<<"The value of a is "<< a <<endl;
+
+
+    
+
+
+
+
+
+
+
+}
