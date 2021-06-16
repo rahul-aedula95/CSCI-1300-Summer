bfb5f93a445b70fc4af481fd5930b6672922ab1d
Rahul Aedula <rahulaedula95@gmail.com>
2021-06-16 16:28:06 -0600

    Week 3

--- Week-3/example4.cpp
+++ Week-3/example4.cpp
@@ -13,6 +13,22 @@ int main(){
     cout << "Output from isupper "<< isupper(var2[0]) << endl;// var2[0] is the value at first index of string so here it is H
     cout << "Output from islower "<< islower(var2[0]) << endl; // var2[0] is the value at first index of string so here it is H
     cout << "Output from islower "<< islower(var2[1]) << endl; // var2[1] is the value at second index of string so here it is i
+
+
+
+    // let us now try to see how it holds up for if else
+
+
+    if (isdigit(var2[0])){    // go ahead and test the different functions above with different combinations. This is just an example
+
+        cout<<"We entered the true branch of if statement"<<endl;
+
+    }
+
+    else{
+
+        cout<<"We entered the false branch of if statement"<<endl;
+    }
     
     return 0;
 
